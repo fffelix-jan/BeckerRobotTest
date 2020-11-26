31b98cecfb1a751ba028861c3481711a9d0aa788
Félix An <cutekids100@gmail.com>
2020-11-26 15:11:52 -0500

    oitugdhh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import becker.robots.*;
 import java.util.Random;
+import java.util.ArrayList;
 import java.lang.Math;
 
 public class Main {
@@ -15,15 +16,26 @@ public class Main {
         }
     }
 
-/*    public static void robotNavToCoords(Robot rMove, int xDest, int yDest) {
+    public static void robotNavToCoords(Robot rMove, int xDest, int yDest) {
         while (rMove.getStreet() != xDest && rMove.getAvenue() != yDest) {
             int bestChoice = Integer.MAX_VALUE;
 
-            int northDist;
+            double northDist;
             robotTurnToFaceDir(rMove, Direction.NORTH);
-
+            if (!rMove.frontIsClear()) {
+                northDist = Double.MAX_VALUE;
+            } else {
+                northDist = Math.sqrt((rMove.getAvenue() - xDest) * (rMove.getAvenue() - xDest) + (rMove.getStreet() - 1 - yDest) * (rMove.getStreet() - 1 - yDest));
+            }
+            double westDist;
+            robotTurnToFaceDir(rMove, Direction.WEST);
+            if (!rMove.frontIsClear()) {
+                westDist = Double.MAX_VALUE;
+            } else {
+                northDist = Math.sqrt((rMove.getAvenue() - 1 - xDest) * (rMove.getAvenue() - 1 - xDest) + (rMove.getStreet() - yDest) * (rMove.getStreet() - yDest));
+            }   // TODO: finish this
         }
-    }*/
+    }
 
     public static void main(String[] args) {
         Random rand = new Random();
@@ -31,16 +43,23 @@ public class Main {
         Robot rb = new Robot(ct, 0, 0, Direction.EAST);
 
         // make 10 garbage things
+        //int[][] garbagePos = {{rand.nextInt(10), rand.nextInt(10)}, {rand.nextInt(10), rand.nextInt(10)}, {rand.nextInt(10), rand.nextInt(10)}, {rand.nextInt(10), rand.nextInt(10)}, {rand.nextInt(10), rand.nextInt(10)}, {rand.nextInt(10), rand.nextInt(10)}, {rand.nextInt(10), rand.nextInt(10)}, {rand.nextInt(10), rand.nextInt(10)}, {rand.nextInt(10), rand.nextInt(10)}, {rand.nextInt(10), rand.nextInt(10)}};
+        ArrayList<int[]> garbagePos = new ArrayList<int[]>();
+        int[] toAddGarbageTemp = new int[2];
+        for (int c = 0; c < 9; c++) {
+            toAddGarbageTemp[0] = rand.nextInt(10);
+            toAddGarbageTemp[1] = rand.nextInt(10);
+            garbagePos.add(toAddGarbageTemp);
+        }
         Thing[] garbage = new Thing[10];
         for (int c = 0; c < 10; c++) {
-            garbage[c] = new Thing(ct, rand.nextInt(10), rand.nextInt(10));
+            garbage[c] = new Thing(ct, garbagePos.get(c)[0], garbagePos.get(c)[1]);
         }
         Wall w = new Wall(ct, 0, 1, Direction.WEST);
 
-        
 
-        for (Thing toPickUpG : garbage) {
-            // while (toPickUpG.get) TODO: write this
+        while (garbagePos.size() > 0) {
+
         }
     }
 }
